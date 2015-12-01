93cf875d7aee4334f7f86a7153780f9ccaf6c72f
Rachel Prince <rachaprince@gmail.com>
2015-12-01 17:49:27 -0500

    json object @survey does exist when tested by pry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -17,6 +18,8 @@ class ApplicationController < ActionController::Base
   		Podio.client.authenticate_with_app('12658551', '775d01a88a884106a3a991c58bdd8e4d')
 
   			render text: "Podio Authenticated!"
+
+  			@survey=Podio::Item.find_basic(339892745)
 
   			# Authentication was a success, now you can start making API calls.
 
